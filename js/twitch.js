37b43d54876575b46cd2d2fd89a532f60441a70a
John Cummings <jcummings5@gmail.com>
2016-04-15 01:58:11 -0400

    messing

--- js/twitch.js
+++ js/twitch.js
@@ -1,12 +1,18 @@
 function twitch() {
 $.getJSON("https://api.twitch.tv/kraken/streams/playcevo?callback=?").done(function(data) {
 			if(data.stream) {
-				document.getElementById("s-online").innerHTML = "Stream is currently <em>ONLINE!</em>";
+				document.getElementById("s-online").innerHTML = "Stream is currently <em>ONLINE playing " + data.stream.game + "!</em>";
 				document.getElementById("s-views").innerHTML = data.stream.viewers;
+				document.getElementById("s-tviews").innerHTML = data.stream.channel.views;
+				document.getElementById("s-followers").innerHTML = data.stream.channel.followers ;
+				document.getElementById("s-preview").innerHTML = "<img src=\"" + data.stream.preview.medium + "\">";
+				
 				
 			} else {
 				document.getElementById("s-online").innerHTML = "Stream is currently <em>OFFLINE!</em>";
 				document.getElementById("s-views").innerHTML = "0";
+				document.getElementById("s-tviews").innerHTML = data.stream.channel.views;
+				document.getElementById("s-followers").innerHTML = data.stream.channel.views;
 			}
 			});
 			}
